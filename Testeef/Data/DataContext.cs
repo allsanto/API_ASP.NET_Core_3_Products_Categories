7a851e7be88aec1a7671e1a89e6c29983ed6a17c
allsanto <55218976+allsanto@users.noreply.github.com>
2020-11-14 23:05:21 -0300

    API_NET_Register

---
+++ Testeef/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Register.Models;
+
+namespace Register.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        { 
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
